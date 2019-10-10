863ce17b2396f2978feccc0e84e377746359093c
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-10-10 15:45:00 +0700

    Add files via upload

---
+++ IF1-101180002-Latihan18-Kelipatan/src/if1/pkg101180002/latihan18/kelipatan/IF1101180002Latihan18Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg101180002.latihan18.kelipatan;
+
+/**
+ * Nama      : Rizky Muslimin
+ * Kelas     : IF-1
+ * NIM       : 10118002
+ * Deskripsi : menghitung kelipatan
+ */
+public class IF1101180002Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    for(double i=3.5; i<=35; i+=3.5){
+        System.out.println(i);
+    }
+    }
+    
+}
